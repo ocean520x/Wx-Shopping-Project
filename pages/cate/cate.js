3709ee817f7d11f490e7f8c78ea8146f38b95d72
ocean_520x <ocean_520x@hchow.icu>
2024-06-03 23:31:46 +0800

    完成了分类页面的开发

--- pages/cate/cate.js
+++ pages/cate/cate.js
@@ -1,66 +1,55 @@
 // pages/cate/cate.js
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        wh: 0,
+        cateList: [],
+        active: 0,
+        cateLevel2: [],
+        scrollTop: 0,// 滚动条距离顶部的距离
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad(options) {
+        const sysInfo = wx.getSystemInfoSync()
+        this.setData({
+            wh: sysInfo.windowHeight
+        })
+        //获取分类列表
+        this.getCateList()
+    },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
+    async getCateList() {
+        try {
+            const res = await wx.fly.get('/api/public/v1/categories')
+            if (res.meta.status !== 200) return wx.toast()
+            this.setData({
+                cateList: res.message,
+                cateLevel2: res.message[0].children
+            })
+        } catch (e) {
+            console.log(e)
+        }
+    },
 
-  },
+    activeChanged(event) {
+        const i = event.currentTarget.dataset.index
+        this.setData({
+            active: i,
+            cateLevel2: this.data.cateList[i].children,
+            scrollTop: this.data.scrollTop ? 0 : 1
+        })
+    },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
-  }
+    gotoGoodsList(event) {
+        const item=event.currentTarget.dataset.item
+        wx.navigateTo({
+          url: '/subpkg/goods_list/goods_list?cid='+item.cat_id,
+        })
+    }
 })
